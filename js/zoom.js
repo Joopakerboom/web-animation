c70f4ef50a7db1cbff09f507ba8a4ceb035e324d
Joop Akerboom <31732324+Joopakerboom@users.noreply.github.com>
2019-05-24 13:32:27 +0200

    Fully responsive + animation

--- js/zoom.js
+++ js/zoom.js
@@ -7,9 +7,6 @@ var undergroundInfo = document.querySelector('body > header div:nth-of-type(1) i
 var Info1 = document.querySelector('main > section:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(1)');
 var Info2 = document.querySelector('main > section:nth-of-type(1) > div:nth-of-type(1) > div:nth-of-type(2)');
 
-var Even = document.querySelector('main > section:nth-of-type(2) #Even');
-var Oneven = document.querySelector('main > section:nth-of-type(2) #Oneven');
-
 var intro = document.querySelector('main > section:nth-of-type(1) > div:nth-of-type(1)');
 var docHeight = intro.offsetHeight;
 
@@ -18,7 +15,10 @@ var artworkText1 = document.querySelector('main > section:nth-of-type(2) > div:n
 var artworkText2 = document.querySelector('main > section:nth-of-type(2) > div:nth-of-type(1) > div > h3:nth-of-type(2)');
 var docHeight2 = artwork.offsetHeight;
 
-//ARTWORK ELEMENTEN
+//ARTWORK ELEMENTS
+var Even = document.querySelector('main > section:nth-of-type(2) #Even');
+var Oneven = document.querySelector('main > section:nth-of-type(2) #Oneven');
+
 var artworkEl1 = document.querySelector('main > section:nth-of-type(2) #London_underground');
 var artworkEl2 = document.querySelector('main > section:nth-of-type(2) #Overlay_london');
 var artworkEl3 = document.querySelector('main > section:nth-of-type(2) #Nose');
@@ -39,7 +39,7 @@ var artworkEl15 = document.querySelector('main > section:nth-of-type(2) #Oneven_
 var oogLinks = document.querySelector('main > section:nth-of-type(2) #Oneven_4');
 var oogRechts = document.querySelector('main > section:nth-of-type(2) #Oneven_5');
 
-//SNOR
+//SNOR ELEMENTS
 var noseGroup1 = document.querySelector('main > section:nth-of-type(2) #Snorharen_Group_1');
 var noseGroup1Detail1 = document.querySelector('main > section:nth-of-type(2) #Snorharen_Group_1 > #Snorharen_1');
 var noseGroup1Detail2 = document.querySelector('main > section:nth-of-type(2) #Snorharen_Group_1 > #Snorharen_2');
@@ -47,33 +47,17 @@ var noseGroup1Detail3 = document.querySelector('main > section:nth-of-type(2) #S
 var noseGroup2 = document.querySelector('main > section:nth-of-type(2) #Snorharen_Group_2');
 var noseHoverCircle = document.querySelector('main > section:nth-of-type(2) > div:nth-of-type(1) > div > div');
 
-//ZOO INHOUD
+//ZOO INHOUD ELEMENTS
 var zooInhoud1 = document.querySelector('main > section:nth-of-type(2) #Zoo1');
 var zooInhoud2 = document.querySelector('main > section:nth-of-type(2) #Zoo2');
 
-//DARK MODE
+//DARK MODE ELEMENTS
 var body = document.querySelector('body');
 var darkModeButton = document.querySelector('body > header > button');
 
 //HEADER SCROLL
-;(function() {
-    var throttle = function(type, name, obj) {
-        var obj = obj || window;
-        var running = false;
-        var func = function() {
-            if (running) { return; }
-            running = true;
-            requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
-                running = false;
-            });
-        };
-        obj.addEventListener(type, func);
-    };
-    throttle ("scroll", "optimizedScroll");
-})();
+// Bron geraadpleegd en getweaked van https://jsfiddle.net/desandro/daZmA/
 
-// bron hier
 window.addEventListener('load', function () {
     window.addEventListener('scroll', function () {
         // normalize scroll position as percentage
@@ -91,97 +75,83 @@ window.addEventListener('load', function () {
 
         var scrolledOneven = -window.scrollY / (docHeight2 - window.innerHeight) * 100 + 100;
         var translateValueOneven = 'translate(' + scrolledOneven + 'vw,0px)';
-        
+
         var scrolledEvenMobile = window.scrollY / (docHeight2 - window.innerHeight) * 150 - 150;
         var translateValueEvenMobile = 'translate(' + scrolledEvenMobile + 'vw,0px)';
 
         var scrolledOnevenMobile = -window.scrollY / (docHeight2 - window.innerHeight) * 150 + 150;
         var translateValueOnevenMobile = 'translate(' + scrolledOnevenMobile + 'vw,0px)';
-        
-//         var scrolledText = window.scrollY / (docHeight2 - window.innerHeight) * 100 - 100;
-//        var translateValueText = 'translate(' + scrolledText + 'vw,0px)';
 
         //      console.log(scrolled);
         //      console.log(blurValue);
         //      console.log(translateValueEven);
         //      console.log(translateValueOneven);
-//        console.log(opacityValue)
+        //       console.log(opacityValue)
 
         box.style.transform = transformValue;
         box2.style.transform = transformValue;
-//              box.style.webkitFilter = blurValue;
 
         Info1.style.opacity = opacityValue;
         Info2.style.opacity = opacityValue;
 
-        if (window.screen.width < 500) {
-        Even.style.transform = translateValueEvenMobile;
-        Oneven.style.transform = translateValueOnevenMobile;
-            }
-        
-//        if (window.screen.width < 720) {
-//        Even.style.transform = translateValueEven;
-//        Oneven.style.transform = translateValueOneven;
-//        }
-        
-        if (window.screen.width > 720) {
-        Even.style.transform = translateValueEven;
-        Oneven.style.transform = translateValueOneven;
+        if (window.screen.width < 720) {
+            Even.style.transform = translateValueEvenMobile;
+            Oneven.style.transform = translateValueOnevenMobile;
         }
-        
-//        artworkText1.style.transform = translateValueText;
-//        artworkText2.style.transform = translateValueText;
+
+        if (window.screen.width > 720) {
+            Even.style.transform = translateValueEven;
+            Oneven.style.transform = translateValueOneven;
+        }
 
     }, false);
 
 }, false);
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     var hoogte = window.pageYOffset + window.innerHeight;
-//    console.log(hoogte);
-  if (hoogte == docHeight2){
-      artworkEl1.classList.add("artappear");
-      artworkEl2.classList.add("artappear");
-      artworkEl3.classList.add("artappear");
-      artworkEl4.classList.add("artappear");
-      artworkEl5.classList.add("artappear");
-      artworkEl6.classList.add("artappear");
-      artworkEl7.classList.add("artappear");
-      
-      darkModeButton.classList.add("buttonappear");
-      
-      artworkText1.classList.add("artappear");
-      artworkText2.classList.add("artappear");
-      
-      artworkEl11.classList.add("staartanimation");
-      artworkEl12.classList.add("staartanimation");
-      artworkEl13.classList.add("staartanimation");
-      artworkEl14.classList.add("staartanimation");
-      artworkEl15.classList.add("staartanimation");
-      }
+    //    console.log(hoogte);
+    if (hoogte == docHeight2) {
+        artworkEl1.classList.add("artappear");
+        artworkEl2.classList.add("artappear");
+        artworkEl3.classList.add("artappear");
+        artworkEl4.classList.add("artappear");
+        artworkEl5.classList.add("artappear");
+        artworkEl6.classList.add("artappear");
+        artworkEl7.classList.add("artappear");
 
-    else {
+        darkModeButton.classList.add("buttonappear");
+
+        artworkText1.classList.add("artappear");
+        artworkText2.classList.add("artappear");
+
+        artworkEl11.classList.add("staartanimation");
+        artworkEl12.classList.add("staartanimation");
+        artworkEl13.classList.add("staartanimation");
+        artworkEl14.classList.add("staartanimation");
+        artworkEl15.classList.add("staartanimation");
+    } else {
         artworkEl1.classList.remove("artappear");
-      artworkEl2.classList.remove("artappear");
-      artworkEl3.classList.remove("artappear");
-      artworkEl4.classList.remove("artappear");
-      artworkEl5.classList.remove("artappear");
-      artworkEl6.classList.remove("artappear");
-      artworkEl7.classList.remove("artappear");
-        
+        artworkEl2.classList.remove("artappear");
+        artworkEl3.classList.remove("artappear");
+        artworkEl4.classList.remove("artappear");
+        artworkEl5.classList.remove("artappear");
+        artworkEl6.classList.remove("artappear");
+        artworkEl7.classList.remove("artappear");
+
         darkModeButton.classList.remove("buttonappear");
-        
+
         artworkText1.classList.remove("artappear");
-      artworkText2.classList.remove("artappear");
-        
+        artworkText2.classList.remove("artappear");
+
         artworkEl11.classList.remove("staartanimation");
-      artworkEl12.classList.remove("staartanimation");
-      artworkEl13.classList.remove("staartanimation");
-      artworkEl14.classList.remove("staartanimation");
-      artworkEl15.classList.remove("staartanimation");
+        artworkEl12.classList.remove("staartanimation");
+        artworkEl13.classList.remove("staartanimation");
+        artworkEl14.classList.remove("staartanimation");
+        artworkEl15.classList.remove("staartanimation");
     }
-    
-},false);
+
+}, false);
 
 
 function darkMode() {
@@ -195,10 +165,10 @@ function darkMode() {
     artworkEl7.classList.add("darkmode");
     artworkEl8.classList.add("darkmode");
     artworkEl9.classList.add("darkmode");
-    
+
     artworkText1.classList.add("darkmode");
     artworkText2.classList.add("darkmode");
-    
+
     oogLinks.classList.add("darkmode");
     oogRechts.classList.add("darkmode");
 }
@@ -206,7 +176,7 @@ function darkMode() {
 function lightMode() {
     body.classList.remove("darkmode");
     darkModeButton.classList.remove("darkmode");
-      artworkEl1.classList.remove("darkmode");
+    artworkEl1.classList.remove("darkmode");
     artworkEl2.classList.remove("darkmode");
     artworkEl3.classList.remove("darkmode");
     artworkEl4.classList.remove("darkmode");
@@ -214,10 +184,10 @@ function lightMode() {
     artworkEl7.classList.remove("darkmode");
     artworkEl8.classList.remove("darkmode");
     artworkEl9.classList.remove("darkmode");
-    
+
     artworkText1.classList.remove("darkmode");
     artworkText2.classList.remove("darkmode");
-    
+
     oogLinks.classList.remove("darkmode");
     oogRechts.classList.remove("darkmode");
 }
@@ -241,52 +211,42 @@ function noseModeHoverOut() {
     noseHoverCircle.style.transform = 'scale(1)';
 }
 
-var more = 2;
+var moreNose = 2;
 
 function noseMode() {
     artworkEl3.classList.add("nosemode");
     noseGroup1.classList.add("nosemode");
-//    noseGroup1Detail1.classList.add("nosemode");
-//    noseGroup1Detail2.classList.add("nosemode");
-//    noseGroup1Detail3.classList.add("nosemode");
-    
     noseGroup2.classList.add("nosemode");
-    
-    noseHoverCircle.style.transform = 'scale(' + more++ + ')';
-    
-    setTimeout(function(){
-    artworkEl3.classList.remove("nosemode");
-    noseGroup1.classList.remove("nosemode");
-   noseGroup2.classList.remove("nosemode");
-},1000);
+
+    noseHoverCircle.style.transform = 'scale(' + moreNose++ + ')';
+
+    setTimeout(function () {
+        artworkEl3.classList.remove("nosemode");
+        noseGroup1.classList.remove("nosemode");
+        noseGroup2.classList.remove("nosemode");
+    }, 1000);
 }
 
-var keydownSide = 1;
+window.addEventListener('keydown', function (e) {
 
-window.addEventListener('keydown', function(e) {
-//    if (e.keyCode === 37) {
-//        Even.style.transform = `translate(-${keydownSide++}vw)`;
-//        
-//    }
-    
     if (e.keyCode === 37) {
         zooInhoud1.classList.add("zoorotationlinks");
-        zooInhoud2.classList.add("zoorotationlinks");  
-        
-        setTimeout(function(){
-   zooInhoud1.classList.remove("zoorotationlinks");
-        zooInhoud2.classList.remove("zoorotationlinks");
-},1500);
+        zooInhoud2.classList.add("zoorotationlinks");
+
+        setTimeout(function () {
+            zooInhoud1.classList.remove("zoorotationlinks");
+            zooInhoud2.classList.remove("zoorotationlinks");
+        }, 1500);
     }
-    
+
     if (e.keyCode === 39) {
         zooInhoud1.classList.add("zoorotationrechts");
         zooInhoud2.classList.add("zoorotationrechts");
-        
-        setTimeout(function(){
-   zooInhoud1.classList.remove("zoorotationrechts");
-        zooInhoud2.classList.remove("zoorotationrechts");
-},1500);
+
+        setTimeout(function () {
+            zooInhoud1.classList.remove("zoorotationrechts");
+            zooInhoud2.classList.remove("zoorotationrechts");
+        }, 1500);
     }
 }, false);
 
@@ -300,7 +260,6 @@ artworkEl3.addEventListener("mouseover", noseModeHover);
 artworkEl3.addEventListener("mouseout", noseModeHoverOut);
 artworkEl3.addEventListener("click", noseMode);
 
-window.addEventListener("scroll", function() {
-    undergroundInfo.style.transform = "rotate("+window.pageYOffset+"deg)";
+window.addEventListener("scroll", function () {
+    undergroundInfo.style.transform = "rotate(" + window.pageYOffset + "deg)";
 });
-
